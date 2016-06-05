758e944b1f06a444f0e51e0746365869a8aefc54
wei zhaodu <921125@163.com>
2016-06-05 23:18:52 +0800

    收藏界面的listview

---
+++ app/src/main/java/com/optimism/tenapp/viewpager/VideoDuixiang.java
@@ -0,0 +1,19 @@
+package com.optimism.tenapp.viewpager;
+
+/**
+ * Created by optimism on 2016/6/4.
+ */
+public class VideoDuixiang {
+    public String title;
+    public String jianjie;
+    public String publishtime;
+    public String publishtimee;
+
+
+
+    public VideoDuixiang(String title, String jianjie, String publishtime) {
+        this.title = title;
+        this.jianjie = jianjie;
+        this.publishtime = publishtime;
+    }
+}
